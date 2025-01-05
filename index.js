9c79dacbdd849f61ecaa908f6ce476bf856aa449
senotron <105975100+senotron@users.noreply.github.com>
2025-01-05 11:10:40 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,30 @@ const { Routes } = require("discord-api-types/v10");
 const { Client, Collection, EmbedBuilder, ModalBuilder, TextInputBuilder,TextInputStyle,ActionRowBuilder,InteractionType} = require('discord.js');
 const client = new Client({ intents: 851 });
 const fs = require("fs");
-const makermrsn = "mrsn.dev"
-const mrsntoken = "MTEzNjk5NzU3NTQyMjEzNjMyMA.Goyucu.NCtP4_d-cP5y0TvhWEG9jBxexfoKjTeY6CEKn4"
+const makermrsn = "senotron"
+const mrsntoken = ""
 const config = require("./config.json")
 
 
 
 global.client = client;
 client.commands = (global.commands = []);
-//#region KOMUTLAR LOAD
-fs.readdir("./komutlar/", (err, files) => {
+
+
+fs.readdir("./commands/", (err, files) => {
   if (err) throw err;
   files.forEach((file) => {
     if (!file.endsWith(".js")) return;
-    let props = require(`./komutlar/${file}`);
+    let props = require(`./commands/${file}`);
     client.commands.push({
       name: props.name.toLowerCase(),
       description: props.description,
       options: props.options,
       type: 1
     })
-    console.log(`${makermrsn}・${props.name} isimli komut yüklendi!`);
   });
 });
-//#endregion
-//#region EVENTS LOAD
+
 fs.readdir("./events/", (_err, files) => {
   files.forEach((file) => {
     if (!file.endsWith(".js")) return;
@@ -39,18 +38,12 @@ fs.readdir("./events/", (_err, files) => {
     });
   });
 });
-//#endregion
-//#region KOMUTLAR SET
-client.on("ready", async () => {
-  console.log(`${makermrsn}・Bot kullanıma hazır!`);
-  client.user.setActivity(`mrsn.dev`, { type: "WATCHING" });
 
-/**
-client.user.setActivity("mrsn.dev", {
-  type: "STREAMING",
-  url: "mrsn.dev"
-});
-**/
+client.on("ready", async () => {
+  console.log(`${makermrsn}・Bot online!`);
+  client.user.setActivity(`https://youtube.com/@senotron`, { type: "WATCHING" });
+
+
   const rest = new REST({ version: "10" }).setToken(mrsntoken);
   try {
     await rest.put(Routes.applicationCommands(client.user.id), {
@@ -60,7 +53,6 @@ client.user.setActivity("mrsn.dev", {
     console.error(error);
   }
 });
-//#endregion
 
 client.login(mrsntoken)
   
